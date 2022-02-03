dfdfab15dd2c06bca97b95058b7ecc0e1574feff
sprocketsullivan <toelch@gmail.com>
2022-02-03 07:39:06 +0000

    added setup rrtools

--- initialise.R
+++ initialise.R
@@ -1,2 +1,6 @@
 install.packages("devtools")
 devtools::install_github("https://github.com/benmarwick/rrtools")
+#enter the name of the package here. Will also be the github repo name later
+#do this despite warnings...
+#a new window will open; copy Setup_comp.R into the working directory of the new folder
+rrtools::use_compendium("pkgname")
